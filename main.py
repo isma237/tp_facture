46384485b92a1f4a9875be622d59972821cc456d
Ismael Gadji <ismael.gadji@orange.com>
2022-02-23 12:10:03 +0100

    Refactorer main.py avec ajout dune fonction getInputData

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ produit={
     "quantite" : 0
 }
 
-listeProduit = []
-prixTotal = 0.0
-nbreProduit = int(input("Combien de produit souhaitez-vous facturer? : "))
-for i in range(0, nbreProduit):
+def getInputData():
     produitEnCours = copy.deepcopy(produit)
     produitEnCours["nom"] = input("Entrer le nom du produit: ")
     produitEnCours["prix"] = float(input("Entrer le prix du produit: "))
     produitEnCours["quantite"] = int(input("Entrer la quantite du produit: "))
+    return produitEnCours
+
+listeProduit = []
+prixTotal = 0.0
+nbreProduit = int(input("Combien de produit souhaitez-vous facturer? : "))
+for i in range(0, nbreProduit):
+    produitEnCours = getInputData()
     listeProduit.append(produitEnCours)
-    prixTotal += produitEnCours["prix"] * produitEnCours["quantite"]
-    
+    prixTotal += produitEnCours["prix"] * produitEnCours["quantite"]    
     print(listeProduit)
 
     
