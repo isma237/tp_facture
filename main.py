cf3e7bba46b4872ff63e7ed6178c5bc25beaf99b
Ismael Gadji <ismael.gadji@orange.com>
2022-02-23 12:06:22 +0100

    Ajout de TIZI

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-#TODO
+import copy
 
-#L'objectif de ce mini TP consite en la génération d'une facture. 
+produit={
+    "nom" : "aucun",
+    "prix" : 0.0,
+    "quantite" : 0
+}
+
+listeProduit = []
+prixTotal = 0.0
+nbreProduit = int(input("Combien de produit souhaitez-vous facturer? : "))
+for i in range(0, nbreProduit):
+    produitEnCours = copy.deepcopy(produit)
+    produitEnCours["nom"] = input("Entrer le nom du produit: ")
+    produitEnCours["prix"] = float(input("Entrer le prix du produit: "))
+    produitEnCours["quantite"] = int(input("Entrer la quantite du produit: "))
+    listeProduit.append(produitEnCours)
+    prixTotal += produitEnCours["prix"] * produitEnCours["quantite"]
+    
+    print(listeProduit)
+
+    
+print(f'Prix total = {prixTotal}')
